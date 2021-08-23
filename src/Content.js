53345b7a6fb22cad8c2ab50115fbbeecef925294
aman-anand17 <legendneverretires@gmail.com>
2021-08-24 02:16:19 +0530

    changed style navbar and api call axios

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,21 @@
-import "./Content.css"
+import "./Content.css";
+import {useEffect,useState} from 'react';
+import axios from "axios";
+
+
+
 function Content(props) {
+    const[title,setTitle] = useState("");
+    useEffect(()=>{
+        async function hello () {
+            const response = await axios.get("http://api.mediastack.com/v1/news?access_key=26c43a644897e596df126fa6cf3a2114&countries=in")
+            console.log(response.data);
+        }
+        /*hello();*/
+    })
     return(
         <div className="content">
+
 
         </div>
     )
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
-import "./Navbar.css"
+import "./Navbar.css";
+import {useState} from "react";
 function Navbar(props){
     return(
+        
         <div className="navbar">
             <div className="navbar-left">
                 <p className="navbar-left-icon">
-                    X
+                <i className="fas fa-newspaper"></i>
 
                 </p>
                 <p className="navbar-left-title">
@@ -14,10 +16,23 @@ function Navbar(props){
 
             </div>
             <div className="navbar-right">
-                <p className="navbar-right-search">
-                    XXX
+                <div className="navbar-right-search">
 
-                </p>
+                    <p className="navbar-right-search-icon">
+                        <i className="fas fa-search"></i>
+
+                    </p>
+
+                    <p className="navbar-right-search-textinput">
+                    
+	                    <input type="search" placeholder="Search..."/>
+
+
+                    </p>
+                    
+                    
+
+                </div>
 
             </div>
             
