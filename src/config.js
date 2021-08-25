3f52f7eb8409c8f7c41d7765250c7ba66c6ef0d4
aman-anand17 <legendneverretires@gmail.com>
2021-08-26 02:32:57 +0530

    added random api key

--- src/config.js
+++ src/config.js
@@ -1,3 +1,25 @@
-// 49cacc541ed4a152d1a723e17cab9825
+//49cacc541ed4a152d1a723e17cab9825
+//a68195d9929f3f275991a5921dc545ec
+//6d9b37d1d0d9fe205ffd9ed516722eb6
+//5567908f8f5d24545893331400ae9ca8
+//88499734e185dc3dc80b7a22c8f1edd9
+/*
 
-export const NEWS_API_KEY = "35e1dc2732e19e473c7e4ff12c409687";
+sachin3@gmail.com
+chutiyaSachindra
+ */
+const apilist = [
+  "49cacc541ed4a152d1a723e17cab9825",
+  "a68195d9929f3f275991a5921dc545ec",
+  "6d9b37d1d0d9fe205ffd9ed516722eb6",
+  "5567908f8f5d24545893331400ae9ca8",
+  "88499734e185dc3dc80b7a22c8f1edd9",
+  "4e018ebde2b16770980db7b3bea5a5ae",
+  "88499734e185dc3dc80b7a22c8f1edd9",
+  "f4cc945d3ce06c5388cc87b3a1b0e34f",
+  "9a1f71660dc1e1aef2d97ac95b50ba05",
+  "35e1dc2732e19e473c7e4ff12c409687",
+];
+let rand_no = Math.floor(Math.random() * apilist.length);
+
+export const NEWS_API_KEY = apilist[rand_no];
