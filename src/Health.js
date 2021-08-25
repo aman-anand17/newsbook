6fa1e2a7316cf5326916df713e3014ded71ea1d0
aman-anand17 <legendneverretires@gmail.com>
2021-08-25 23:50:52 +0530

    publish router

--- src/Health.js
+++ src/Health.js
@@ -1,25 +1,25 @@
-import { useState, useEffect } from 'react';
-import { NEWS_API_KEY } from './config';
-import ArticleList from './ArticleList.js';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import { NEWS_API_KEY } from "./config";
+import ArticleList from "./ArticleList.js";
+import axios from "axios";
 
 const url = `http://api.mediastack.com/v1/news?access_key=${NEWS_API_KEY}&countries=in&languages=en&categories=health`;
 
-function Health(props){
-    const [ articles, setArticles] = useState([]);
-    const getData = async () => {
-        return axios
-          .get(`${url}`)
-          .then((response) =>setArticles(response.data.data));
-      };
-      useEffect(() => {
-		getData();
-	},[]);
-    return(
-        <div>
-            Health
-            <ArticleList articles ={articles}/>
-        </div>
-    )
+function Health(props) {
+  const [articles, setArticles] = useState([]);
+  const getData = async () => {
+    return axios
+      .get(`${url}`)
+      .then((response) => setArticles(response.data.data));
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div>
+      Health
+      <ArticleList articles={articles} />
+    </div>
+  );
 }
 export default Health;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {BrowserRouter} from "react-router-dom";
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-   <React.StrictMode>
-     <BrowserRouter>
-     <App />
-     </BrowserRouter>
-    
-    </React.StrictMode>,
-  
-  
-  document.getElementById('root')
-);
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
 
+  document.getElementById("root")
+);
