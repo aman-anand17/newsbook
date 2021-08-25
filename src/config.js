2c5a3746abbaeab6e45366a3ae217be9cba453f5
aman-anand17 <legendneverretires@gmail.com>
2021-08-25 16:26:34 +0530

    new api key added

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const NEWS_API_KEY = "26c43a644897e596df126fa6cf3a2114";
+/*export const NEWS_API_KEY = "26c43a644897e596df126fa6cf3a2114";*/
+export const NEWS_API_KEY = '9f2264eaf507e79843d050a5d5c01f53'
