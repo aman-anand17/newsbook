dc3c796101cf5d9fef745d11e6db603d16fea25c
Sourabh Raha <70322684+sourabhraha@users.noreply.github.com>
2022-05-27 20:29:00 +0530

    Update config.js
    Updated APIs

--- src/config.js
+++ src/config.js
@@ -1,18 +1,10 @@
-//49cacc541ed4a152d1a723e17cab9825
-//a68195d9929f3f275991a5921dc545ec
-//6d9b37d1d0d9fe205ffd9ed516722eb6
-//5567908f8f5d24545893331400ae9ca8
-//88499734e185dc3dc80b7a22c8f1edd9
-/*
 
-sachin3@gmail.com
-SachindraChutiya
- */
-const apilist = ["9403318cd317a48a92513c72b921098d",  //yoyo6005
-                    'b661d3e0618798169d3ae4fc9e1cf844', //bbcc
-                    'b661d3e0618798169d3ae4fc9e1cf844' /// 
-    
-                    
+const apilist = [
+  "73be567473614bfdf5ec112c52088363",
+  "ca95c867f3bf4cede21557475a2f10fb",
+  "44ecd5895721905dae6e0625c69fca96",
+  "7045d5c0aeef7dc46b80c21b36bbbd3c",
+  "4c03eca336175a11538c4b6f30b0d518",
 ];
 let rand_no = Math.floor(Math.random() * apilist.length);
 
